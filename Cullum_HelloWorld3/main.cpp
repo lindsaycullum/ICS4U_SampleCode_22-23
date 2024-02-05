676eccb92a82ad5f41ad550ff858148011af3ae9
lindsaycullum <lindsay.cullum@ocdsb.ca>
2024-02-05 12:35:59 -0500

    Added hello world program

---
+++ Cullum_HelloWorld3/main.cpp
@@ -0,0 +1,19 @@
+//Cullum - Hello World Program
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World" << endl;
+    cout << "There is more." << endl;
+
+    float age;
+    cout << "What is your age?" << endl;
+    cin >> age;
+
+    cout << "Your age is: " << age << "." << endl;
+
+    return 0;
+
+}
